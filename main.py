f3fcfa099adb6dd30d8ad495066b9f3119001d80
Steven Turner <sturner@cuhsd.org>
2020-09-21 18:31:33 +0000

    Added starter info for step 4.

---
+++ main.py
@@ -0,0 +1,14 @@
+num = int(input("Enter a number -> "))
+if (num >= 90):
+  print("Input is 90 or more")
+else:
+    if (num >= 80):
+        print("Input is 80 or more")
+    else:
+      if(num >= 70):
+        print("Input is 70 or more")
+      else:
+        if (num >= 60):
+          print("Input is 60 or more")
+        else:
+          print("Input is less than 60")
